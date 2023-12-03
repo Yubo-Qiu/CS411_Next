ce777c26fecd50dd917333f9da85dc928ece3f48
Yubo-Qiu <yubo@bu.edu>
2023-12-03 04:22:46 -0500

    api almost done

--- cs411/src/app/dashboard/page.js
+++ cs411/src/app/dashboard/page.js
@@ -1,15 +1,83 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Navbar from '../components/Navbar'; 
-import Footer from '../components/Footer'; 
+"use client"
+
+
 import Userbar from '../components/Userbar'; 
-import Sidebarbar from '../components/Userbar'; 
-import dynamic from 'next/dynamic';
-import { UserAuth } from "../context/AuthContext";
 import Sidebar from '../components/Sidebar';
 import styles from './page.module.css'; 
 
+import React, { useEffect, useState } from 'react';
+import Chart from 'react-apexcharts';
+import fetchStockData from './api'; 
+
+
+
 const Dashboard = () => {
+
+  const [graphData, setGraphData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiData = await fetchStockData('TSLA'); // Fetch the data
+        // Transform the data to fit the chart requirements
+        const transformedData = Object.entries(apiData['Time Series (5min)']).map(([date, data]) => { 
+          return {
+            x: new Date(date), // use 'date' here, which represents the timestamp
+            y: parseFloat(data['4. close']) // parse the closing value for the y-axis
+          };
+        }).reverse();
+        setGraphData([{ data: transformedData }]); // Set the transformed data in state
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const chartOptions = {
+    chart: {
+      id: 'basic-line',
+      type: 'line',
+      height: 350,
+      toolbar: {
+        show: false // Hides the toolbar for more space
+      },
+    },
+    grid: {
+      padding: {
+        left: 10, // Adjust left padding
+        right: 10, // Adjust right padding
+        paddingBottom: 20,
+      },
+    },
+    xaxis: {
+      labels: {
+        formatter: function (val) {
+          return new Date(val).toLocaleDateString('en-US', { month: 'short', day: 'numeric' }); // Formats the date as "Jan 1"
+        },
+        trim: true, // Trims the labels if they overflow
+        rotate: -45, // Rotates labels
+        minHeight: 100,
+      },
+    },
+    stroke: {
+      curve: 'smooth',
+    },
+    title: {
+      text: 'Stock for TSLA for today',
+      align: 'center',
+    },
+    markers: {
+      size: 1,
+    },
+    tooltip: {
+      shared: true,
+      intersect: false,
+    },
+  };
+
+
   return (
     <>
       <div className={styles.pageContainer}>
@@ -29,6 +97,9 @@ const Dashboard = () => {
           </div>  
           <div className={styles.graphRectangle}>
             {/* Your graph content goes here */}
+            {typeof window !== "undefined" && (
+              <Chart options={chartOptions} series={graphData} type="line" />
+            )}
           </div>
           <div className={styles.buttonContainer}>
             <button className={styles.actionButton}>Trade History</button>
--- cs411/src/app/news/page.js
+++ cs411/src/app/news/page.js
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router'; // Corrected import
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import styles from './page.module.css';
 import fetchNewsData from './api'; 
 
@@ -26,10 +27,10 @@ const News = () => {
     return (
         <>
             <Navbar />
-            <div className={styles.background}>
-                <div className={styles.formWrapper}>
-                <h1>BBC News</h1>
-                <div className={styles.contentContainer}> {/* Apply the new container here */}
+                <div className={styles.combinedWrapper} >
+                <div className={styles.overlay} >
+                <div className={styles.contentContainer} > 
+                <h3 className={styles.newsHeading}>Some "Global <span className={styles.highlight}>News</span>"😏</h3>
                         {articles.map((article, index) => (
                             <div key={index} className={styles.newsItem}>
                                 <h2>{article.title}</h2>
@@ -39,7 +40,8 @@ const News = () => {
                         ))}
                     </div>
                 </div>
-            </div>
+                </div>
+            <Footer />
         </>
     );
 }
